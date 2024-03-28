c3c10aa17bd99f2f0622fdab0431003ea21695bc
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-28 17:07:28 -0600

    Create collections

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const { EventsLoader } = require('./handlers/EventHandler.js');
 const { CommandsLoader } = require('./handlers/CommandHandler.js');
 const config = require('../config/monitor.config.js');
@@ -7,6 +7,9 @@ const client = new Client({
     intents: [Object.keys(GatewayIntentBits)],
     partials: [Object.keys(Partials)],
 });
+
+client.commands = new Collection();
+client.commandArray = [];
 
 client.login(config.bot.token).then(() => {
     EventsLoader(client);
