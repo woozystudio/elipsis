aff73281ba563a1b68dfba0061a5c9ab5738c8e4
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-28 15:38:53 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import { Client, GatewayIntentBits, Partials } from "discord.js";
-import config from '../config/monitor.config.js';
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { EventsLoader } = require('./handlers/EventHandler.js');
+const { CommandsLoader } = require('./handlers/CommandHandler.js');
+const config = require('../config/monitor.config.js');
 
 const client = new Client({
     intents: [Object.keys(GatewayIntentBits)],
     partials: [Object.keys(Partials)],
 });
 
-client.login(config.bot.token);
+client.login(config.bot.token).then(() => {
+    EventsLoader(client);
+    CommandsLoader(client);
+});
