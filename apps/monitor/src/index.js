d51430821ce7edbebfe4f7d1a2e8d8c610f00983
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-28 21:05:31 -0600

    Update config

--- apps/monitor/src/index.js
+++ apps/monitor/src/index.js
@@ -1,7 +1,7 @@
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const { EventsLoader } = require('./handlers/EventHandler.js');
 const { CommandsLoader } = require('./handlers/CommandHandler.js');
-const config = require('../config/monitor.config.js');
+const config = require('../../../config/monitor.config.js');
 
 const client = new Client({
     intents: [Object.keys(GatewayIntentBits)],
