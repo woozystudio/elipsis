8d87a3c12f57a7bb23c33d7ec18df125d95fa4d2
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-29 09:30:25 -0600

    Create Handler class

---
+++ apps/worf/src/classes/Handler.ts
@@ -0,0 +1,33 @@
+import IHandler from "../interfaces/Handler";
+import path from "path";
+import { glob } from "glob";
+import WorfClient from "./Client";
+import Event from "./Event";
+
+export default class Handler implements IHandler {
+    client: WorfClient;
+    constructor(client: WorfClient) {
+        this.client = client;
+    }
+
+    async LoadEvents() {
+        const files = (await glob(`build/events/**/*.js`)).map(filePath => path.resolve(filePath));
+
+        files.map(async (file: string) => {
+            const event: Event = new(await import(file)).default(this.client);
+
+            if(!event.name)
+                return delete require.cache[require.resolve(file)] && console.log(`${file.split("/").pop()} does not have name`);
+
+            const execute = (...args: any) => event.Execute(...args);
+
+
+            //@ts-ignore
+            if(event.once) this.client.once(event.name, execute);
+            //@ts-ignore
+            else this.client.on(event.name, execute);
+
+            return delete require.cache[require.resolve(file)];
+        });
+    }
+}
