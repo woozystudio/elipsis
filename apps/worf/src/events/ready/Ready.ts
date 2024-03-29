d94a8c760a0bf65de8516a48931f7a80ffedcddd
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-29 11:05:48 -0600

    Add commands loader functions

--- apps/worf/src/events/ready/Ready.ts
+++ apps/worf/src/events/ready/Ready.ts
@@ -1,6 +1,8 @@
-import { Events } from "discord.js";
+import { Collection, Events, REST, Routes } from "discord.js";
 import WorfClient from "../../classes/Client";
 import Event from "../../classes/Event";
+import Command from "../../classes/Command";
+import 'colors';
 
 export default class Ready extends Event {
     constructor(client: WorfClient) {
@@ -11,7 +13,33 @@ export default class Ready extends Event {
         })
     }
 
-    Execute() {
-        console.log(`Logged in ${this.client.user?.tag}.`)
+    async Execute() {
+        console.log(`Logged as ${this.client.user?.tag}`.green);
+
+        const rest = new REST().setToken(this.client.config.token);
+
+        const commands: object[] = this.GetJson(this.client.commands);
+
+        const setCommands: any = await rest.put(Routes.applicationGuildCommands(this.client.config.discordClientId, this.client.config.guildId), {
+            body: commands
+        });
+
+        console.log(`Successfully set ${setCommands.length} commands!`.yellow);
+    }
+
+    private GetJson(commands: Collection<string, Command>): object[] {
+        const data: object[] = [];
+
+        commands.forEach(command => {
+            data.push({
+                name: command.name,
+                description: command.description,
+                options: command.options,
+                userPermissions: command.userPermissions.toString(),
+                dmPermissions: command.dmPermissions,
+            })
+        });
+
+        return data;
     }
 }
