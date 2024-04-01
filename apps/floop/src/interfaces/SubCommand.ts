9d10c5613e2c41e826f9217a30dd013d107938d3
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-01 09:00:29 -0600

    Delete Floop SubCommand.ts

--- apps/floop/src/interfaces/SubCommand.ts
+++
@@ -1,9 +0,0 @@
-import { ChatInputCommandInteraction } from "discord.js";
-import FloopClient from "../classes/Client";
-
-export default interface ISubCommand {
-    client: FloopClient;
-    name: string;
-
-    Execute(interaction: ChatInputCommandInteraction): void;
-}
