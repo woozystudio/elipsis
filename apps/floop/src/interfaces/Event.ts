532310935fa564f9e2bcf041cb86e1448457f087
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-01 09:00:05 -0600

    Delete Floop Event.ts

--- apps/floop/src/interfaces/Event.ts
+++
@@ -1,9 +0,0 @@
-import { Events } from "discord.js";
-import FloopClient from "../classes/Client";
-
-export default interface IEvent {
-    client: FloopClient;
-    name: Events;
-    description: string;
-    once: boolean;
-}
