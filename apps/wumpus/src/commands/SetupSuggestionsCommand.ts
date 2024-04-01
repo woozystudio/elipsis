3d8024ff6f19b6fc86e902c2e6c4758988b15847
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-01 10:05:38 -0600

    Create SetupSuggestionsCommand.ts

---
+++ apps/wumpus/src/commands/SetupSuggestionsCommand.ts
@@ -0,0 +1,48 @@
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, PermissionFlagsBits } from "discord.js";
+import WumpusClient from "../classes/Client";
+import Command from "../classes/Command";
+import Category from "../enums/Category";
+import SuggestionSetup from "../database/models/Suggestion";
+
+export default class SuggestionSetupCommand extends Command {
+    constructor(client: WumpusClient) {
+        super(client, {
+            name: "setup-suggestions",
+            description: "Configures the suggestion system on the server.",
+            category: Category.Utilities,
+            userPermissions: PermissionFlagsBits.ManageGuild,
+            dmPermissions: true,
+            development: false,
+            cooldown: 2,
+            options: [
+                {
+                    name: "channel",
+                    description: "Select the suggestion channel.",
+                    type: ApplicationCommandOptionType.Channel,
+                    required: true
+                }
+            ],
+        });
+    }
+
+    async Execute(interaction: ChatInputCommandInteraction) {
+        const suggestionChannel = interaction.options.getChannel('channel');
+
+        try {
+            if(suggestionChannel && interaction.guild) {
+                const data = await SuggestionSetup.findOneAndUpdate({ GuildID: interaction.guild.id }, {
+                    Channel: suggestionChannel.id
+                },
+                {
+                    new: true,
+                    upsert: true
+                });
+
+                interaction.reply({ content: "✅ `|` The suggestion system has been configured correctly!" });
+            }
+        } catch (err) {
+            console.error(err);
+            interaction.reply({ content: "❌ `|` There was an error configuring the suggestion system." });
+        }
+    }
+}
