d285720622b5169759f9b87336e0a954845697a9
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-31 19:19:46 -0600

    Create Ready.ts

---
+++ apps/floop/src/events/ready/Ready.ts
@@ -0,0 +1,52 @@
+import { Collection, Events, REST, Routes } from "discord.js";
+import FloopClient from "../../classes/Client";
+import Event from "../../classes/Event";
+import Command from "../../classes/Command";
+import 'colors';
+
+export default class Ready extends Event {
+    constructor(client: FloopClient) {
+        super(client, {
+            name: Events.ClientReady,
+            description: "Ready Event",
+            once: true
+        })
+    }
+
+    async Execute() {
+        console.log(`Logged as ${this.client.user?.tag}`.green);
+
+        const clientId = this.client.development ? this.client.config.developmentBotId : this.client.config.botId;
+        const rest = new REST().setToken(this.client.config.token);
+
+        if(!this.client.development) {
+            const globalCommands: any = await rest.put(Routes.applicationCommands(clientId), {
+                body: this.GetJson(this.client.commands.filter(command => !command.development))
+            });
+
+            console.log(`Successfully loaded ${globalCommands.length} global application commands.`.yellow)
+        }
+
+        const devCommands: any = await rest.put(Routes.applicationGuildCommands(clientId, this.client.config.developmentGuildId), {
+            body: this.GetJson(this.client.commands.filter(command => command.development))
+        });
+
+        console.log(`Successfully loaded ${devCommands.length} development application commands.`.red)
+    }
+
+    private GetJson(commands: Collection<string, Command>): object[] {
+        const data: object[] = [];
+
+        commands.forEach(command => {
+            data.push({
+                name: command.name,
+                description: command.description,
+                options: command.options,
+                userPermissions: command.userPermissions.toString(),
+                dmPermissions: command.dmPermissions,
+            })
+        });
+
+        return data;
+    }
+}
