bec82a620ad125e32f3655243b023034ea197950
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-03 08:03:42 -0600

    Create TicketsActionsCreate.ts

---
+++ apps/floop/src/events/interactionCreate/TicketActionsCreate.ts
@@ -0,0 +1,54 @@
+import { ButtonInteraction, ChatInputCommandInteraction, EmbedBuilder, Events, PermissionFlagsBits } from "discord.js";
+import FloopClient from "../../classes/Client";
+import Event from "../../classes/Event";
+import TicketSetup from "../../database/models/TicketSetup";
+import Ticket from "../../database/models/Ticket";
+import Color from "../../enums/Color";
+
+export default class InteractionCreate extends Event {
+    constructor(client: FloopClient) {
+        super(client, {
+            name: Events.InteractionCreate,
+            description: "TicketResponse Event",
+            once: false
+        });
+    }
+
+    async Execute(interaction: ChatInputCommandInteraction) { 
+        if(interaction.isButton()) {
+            const buttons = interaction as ButtonInteraction;
+
+            if(buttons.guild && buttons.member && buttons.channel) {
+                if(!buttons.customId.includes("close")) return;
+                const docs = await TicketSetup.findOne({ GuildId: buttons.guild.id });
+                const data = await Ticket.findOne({ GuildID: buttons.guild.id });
+                if(!buttons.guild.members.me?.permissions.has(PermissionFlagsBits.ManageChannels)) return buttons.reply({ content: "❌ `|` The bot needs permission from `ManageChannels` to do this action." });
+                if(data) {
+                    switch (buttons.customId) {
+                        case "close":
+                            if (data.Closed === true) return buttons.reply({ content: `✅ \`|\` This ticket is already getting deleted...`, ephemeral: true });
+        
+                            await Ticket.updateOne({ ChannelID: buttons.channel.id }, { Closed: true });
+                            const DeletedTicket = new EmbedBuilder()
+                            .setDescription(`The ticket will be deleted in 5s...`)
+                            .setColor(Color.Danger)
+        
+                            buttons.channel.send({ embeds: [DeletedTicket] });
+                            setTimeout(function () {
+                                if(buttons.channel) {
+                                    buttons.channel.delete();
+                                }
+                            }, 5000)
+        
+                            buttons.reply({ content: `✅ \`|\` Your ticket is being prepared for closing.` });
+                            break;
+                        
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+    }
+}
