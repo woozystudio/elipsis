ac194e6a4ce5ae7728015276283b96ae1cdf7675
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-04 08:33:39 -0600

    Delete pinky SubCommand.ts

--- apps/pinky/src/interfaces/SubCommand.ts
+++
@@ -1,9 +0,0 @@
-import { ChatInputCommandInteraction } from "discord.js";
-import PinkyClient from "../classes/Client";
-
-export default interface ISubCommand {
-    client: PinkyClient;
-    name: string;
-
-    Execute(interaction: ChatInputCommandInteraction): void;
-}
