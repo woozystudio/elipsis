0e73472f81b79c433c784ca68301f318e3af2533
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-04 11:37:26 -0600

    Create PollCommand.ts

---
+++ apps/worf/src/commands/PollCommand.ts
@@ -0,0 +1,97 @@
+import { ApplicationCommandOptionType, ChannelType, ChatInputCommandInteraction, EmbedBuilder, PermissionFlagsBits, TextChannel } from "discord.js";
+import WorfClient from "../classes/Client";
+import Command from "../classes/Command";
+import Category from "../enums/Category";
+import Color from "../enums/Color";
+
+export default class PollCommand extends Command {
+    constructor(client: WorfClient) {
+        super(client, {
+            name: "poll",
+            description: "Create a poll on the server.",
+            category: Category.Utilities,
+            userPermissions: PermissionFlagsBits.UseApplicationCommands,
+            dmPermissions: true,
+            development: true,
+            cooldown: 2,
+            options: [
+                {
+                    name: "topic",
+                    description: "Write a title for your poll.",
+                    required: true,
+                    type: ApplicationCommandOptionType.String,
+                },
+                {
+                    name: "description",
+                    description: "Write a description for your poll.",
+                    required: true,
+                    type: ApplicationCommandOptionType.String,
+                },
+                {
+                    name: "vote-type",
+                    description: "Select a voting type.",
+                    required: true,
+                    type: ApplicationCommandOptionType.String,
+                    choices: [
+                        { name: "✅ & ❌", value: "check-cross" },
+                        { name: "👍 & 👎", value: "good-bad" },
+                        { name: "⬆ & ⬇", value: "up-down" }
+                    ]
+                },
+                {
+                    name: "channel",
+                    description: "Select a channel to send your poll.",
+                    required: false,
+                    type: ApplicationCommandOptionType.Channel,
+                    channel_types: [ChannelType.GuildText]
+                }
+            ],
+        });
+    }
+
+    async Execute(interaction: ChatInputCommandInteraction) {
+        const topic = interaction.options.getString('topic');
+        const description = interaction.options.getString('description');
+        const votetype = interaction.options.getString('vote-type');
+        const channel = interaction.options.getChannel('channel');
+
+        const PollEmbed = new EmbedBuilder()
+        .setAuthor({ iconURL: `${interaction.user.displayAvatarURL()}`, name: interaction.user.username })
+        .setTitle(topic)
+        .setThumbnail(`${interaction.guild?.iconURL()}`)
+        .setDescription(description)
+        .setColor(Color.Embed)
+        .setTimestamp()
+
+        await interaction.reply({ content: "✅ `|` The poll has been sent successfully!", ephemeral: true });
+
+        if(channel) {
+            const Channel = await this.client.channels.cache.get(channel.id) as TextChannel;
+            const Reaction = await Channel.send({ embeds: [PollEmbed] });
+
+            if (votetype === "check-cross") {
+                Reaction.react(`✅`);
+                Reaction?.react(`❌`);
+            } else if (votetype === "good-bad") {
+                Reaction?.react(`👍`);
+                Reaction?.react(`👎`);
+            } else if (votetype === "up-down") {
+                Reaction?.react(`⬆`);
+                Reaction?.react(`⬇`);
+            }
+
+        } else {
+            const Reaction = await interaction.channel?.send({ embeds: [PollEmbed] });
+            if (votetype === "check-cross") {
+                Reaction?.react(`✅`);
+                Reaction?.react(`❌`);
+            } else if (votetype === "good-bad") {
+                Reaction?.react(`👍`);
+                Reaction?.react(`👎`);
+            } else if (votetype === "up-down") {
+                Reaction?.react(`⬆`);
+                Reaction?.react(`⬇`);
+            }
+        }
+    }
+}
