e212942ba92b5f5492619f8f7dceee04294ece7c
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-04 12:15:57 -0600

    Update TicketActionsCreate.ts (#10)

--- apps/floop/src/events/interactionCreate/TicketActionsCreate.ts
+++ apps/floop/src/events/interactionCreate/TicketActionsCreate.ts
@@ -4,6 +4,7 @@ import Event from "../../classes/Event";
 import TicketSetup from "../../database/models/TicketSetup";
 import Ticket from "../../database/models/Ticket";
 import Color from "../../enums/Color";
+import Symbols from "../../enums/Symbols";
 
 export default class InteractionCreate extends Event {
     constructor(client: FloopClient) {
@@ -22,11 +23,11 @@ export default class InteractionCreate extends Event {
                 if(!buttons.customId.includes("close")) return;
                 const docs = await TicketSetup.findOne({ GuildId: buttons.guild.id });
                 const data = await Ticket.findOne({ GuildID: buttons.guild.id });
-                if(!buttons.guild.members.me?.permissions.has(PermissionFlagsBits.ManageChannels)) return buttons.reply({ content: "❌ `|` The bot needs permission from `ManageChannels` to do this action." });
+                if(!buttons.guild.members.me?.permissions.has(PermissionFlagsBits.ManageChannels)) return buttons.reply({ content: `${Symbols.Error} The bot needs permission from \`ManageChannels\` to do this action.` });
                 if(data) {
                     switch (buttons.customId) {
                         case "close":
-                            if (data.Closed === true) return buttons.reply({ content: `✅ \`|\` This ticket is already getting deleted...`, ephemeral: true });
+                            if (data.Closed === true) return buttons.reply({ content: `${Symbols.Success} This ticket is already getting deleted...`, ephemeral: true });
         
                             await Ticket.updateOne({ ChannelID: buttons.channel.id }, { Closed: true });
                             const DeletedTicket = new EmbedBuilder()
@@ -40,7 +41,7 @@ export default class InteractionCreate extends Event {
                                 }
                             }, 5000)
         
-                            buttons.reply({ content: `✅ \`|\` Your ticket is being prepared for closing.` });
+                            buttons.reply({ content: `${Symbols.Success} Your ticket is being prepared for closing.` });
                             break;
                         
                         default:
