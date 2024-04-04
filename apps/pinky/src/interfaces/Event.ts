efbfc4811a0f6e2ba0c2e33d0ba7efb6332b4f2e
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-04 08:33:17 -0600

    Delete pinky Event.ts

--- apps/pinky/src/interfaces/Event.ts
+++
@@ -1,9 +0,0 @@
-import { Events } from "discord.js";
-import PinkyClient from "../classes/Client";
-
-export default interface IEvent {
-    client: PinkyClient;
-    name: Events;
-    description: string;
-    once: boolean;
-}
