cd2b1a459e69163be3fa77bae8818ebba0f5e2ad
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-04 19:12:56 -0600

    Create UnlockCommand.ts

---
+++ apps/floop/src/commands/UnlockCommand.ts
@@ -0,0 +1,42 @@
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, PermissionFlagsBits, TextChannel } from "discord.js";
+import FloopClient from "../classes/Client";
+import Command from "../classes/Command";
+import Category from "../enums/Category";
+import Ticket from "../database/models/Ticket";
+import Symbols from "../enums/Symbols";
+
+export default class LockCommand extends Command {
+    constructor(client: FloopClient) {
+        super(client, {
+            name: "unlock",
+            description: "Unlocks a ticket from the user who created it.",
+            category: Category.Utilities,
+            userPermissions: PermissionFlagsBits.ManageChannels,
+            dmPermissions: true,
+            development: false,
+            cooldown: 2,
+            options: [],
+        });
+    }
+
+    async Execute(interaction: ChatInputCommandInteraction) {
+        if(interaction.channel && interaction.memberPermissions) {
+            if (!interaction.memberPermissions.has(PermissionFlagsBits.ManageChannels)) return interaction.reply({ content: `You are not allowed to unlock a ticket.`, ephemeral: true });
+            const data = await Ticket.findOne({ ChannelID: interaction.channel.id });
+            if(!data) return;
+
+            if (data.Locked === false) return interaction.reply({ content: `${Symbols.Error} The ticket has already been unlocked.`, ephemeral: true });
+            await Ticket.updateOne({ ChannelID: interaction.channel.id }, { Locked: false });
+            
+            data.MembersID.forEach(async (user) => {
+                if(interaction.channel) {
+                    const channel = interaction.channel as TextChannel;
+                    const member: any = await interaction.guild?.members.fetch(user);
+                    await channel.permissionOverwrites.edit(member, { SendMessages: true })
+                }
+            });
+
+            return interaction.reply({ content: `${Symbols.Success} The ticket has been successfully unlocked!` });
+        }
+    }
+}
