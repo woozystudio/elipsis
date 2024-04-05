30a401acdf8a84a7051b50e91396f1f326018bfe
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-05 08:20:58 -0600

    Update symbols

--- apps/wumpus/src/enums/Symbols.ts
+++ apps/wumpus/src/enums/Symbols.ts
@@ -1,6 +1,6 @@
 enum Symbols {
-    Success = "✅ \`|\`",
-    Error = "❌ \`|\`",
+    Success = "<:checkmark:1219293026489339914> \`|\`",
+    Error = "<:crossmark:1219293029681201242> \`|\`",
     Warning = "⚠ \`|\`",
 }
 
