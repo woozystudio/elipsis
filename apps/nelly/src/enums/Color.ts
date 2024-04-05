07f7baf4df17f25e296ab1519556b5560445732a
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-05 07:48:23 -0600

    Create Color.ts

---
+++ apps/nelly/src/enums/Color.ts
@@ -0,0 +1,12 @@
+enum Color {
+    Embed = 0x2B2D31,
+    Danger = 0xf04747,
+    Severe = 0xfaa61a,
+    Warning = 0xFFD51C,
+    Success = 0x43b581,
+    Note = 0x1CA0FF,
+    Important = 0xB71CFF,
+    Sponsors = 0xFF1CE0,
+}
+
+export default Color;
