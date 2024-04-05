35504d4bb07a135a2c47e1d9f98fe3c5bf6fc335
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-05 13:01:32 -0600

    Create UnlockCommand.ts

---
+++ apps/nelly/src/commands/UnlockCommand.ts
@@ -0,0 +1,47 @@
+import { ApplicationCommandOptionType, ChannelType, ChatInputCommandInteraction, EmbedBuilder, PermissionFlagsBits, TextChannel } from "discord.js";
+import NellyClient from "../classes/Client";
+import Command from "../classes/Command";
+import Category from "../enums/Category";
+import Color from "../enums/Color";
+import Symbols from "../enums/Symbols";
+
+export default class UnlockCommand extends Command {
+    constructor(client: NellyClient) {
+        super(client, {
+            name: "unlock",
+            description: "Unlocks a text channel for server members.",
+            category: Category.Utilities,
+            userPermissions: PermissionFlagsBits.ManageChannels,
+            dmPermissions: true,
+            development: false,
+            cooldown: 2,
+            options: [
+                {
+                    name: "channel",
+                    description: "Select the channel you want to unlock.",
+                    required: true,
+                    type: ApplicationCommandOptionType.Channel,
+                    channel_types: [ChannelType.GuildText]
+                }
+            ],
+        });
+    }
+
+    async Execute(interaction: ChatInputCommandInteraction) {
+        const channel = interaction.options.getChannel('channel') as TextChannel;
+        const everyone = interaction.guild?.roles.everyone;
+        
+        if(channel && everyone && interaction.guild) {
+            channel.permissionOverwrites.edit(everyone.id, { CreatePublicThreads: true, AddReactions: true, SendMessages: true });
+
+            const UnlockEmbed = new EmbedBuilder()
+            .setColor(Color.Success)
+            .setDescription(`${Symbols.Success} The channel ${channel} has been successfully unlocked.`)
+    
+            interaction.reply({ embeds: [UnlockEmbed] });
+            
+        } else {
+            throw new Error("CommandError: An error occurred in Unlock command.")
+        }
+    }
+}
