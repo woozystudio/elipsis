16852a32e3fa996c3add540750e6a31bcc42a1a3
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-07 16:51:39 -0600

    Create ApplicationActions.ts

---
+++ apps/vroom/src/events/interactionCreate/ApplicationActions.ts
@@ -0,0 +1,57 @@
+import { ButtonInteraction, ChatInputCommandInteraction, EmbedBuilder, Events, PermissionFlagsBits } from "discord.js";
+import VroomClient from "../../classes/Client";
+import Event from "../../classes/Event";
+import Application from "../../database/models/Application";
+import Postulation from "../../database/models/Postulation";
+import Color from "../../enums/Color";
+import Symbols from "../../enums/Symbols";
+
+export default class ApplicationResponse extends Event {
+    constructor(client: VroomClient) {
+        super(client, {
+            name: Events.InteractionCreate,
+            description: "ApplicationResponse Event",
+            once: false
+        });
+    }
+
+    async Execute(interaction: ChatInputCommandInteraction) { 
+        if(interaction.isButton()) {
+            const buttons = interaction as ButtonInteraction;
+
+            if(buttons.guild && buttons.member && buttons.channel && buttons.memberPermissions ) {
+                if(!["close"].includes(buttons.customId)) return;
+
+                const data = await Postulation.findOne({ GuildID: buttons.guild.id });
+                if(!buttons.guild.members.me?.permissions.has(PermissionFlagsBits.ManageChannels)) return buttons.reply({ content: `${Symbols.Error} The bot needs permission from \`ManageChannels\` to do this action.` });
+                if(data) {
+                    switch (buttons.customId) {
+                        case "close":
+                            if (!buttons.memberPermissions.has(PermissionFlagsBits.ManageChannels)) return buttons.reply({ content: `${Symbols.Error} You do not have permission to close this application.`, ephemeral: true });
+                            if (data.Closed === true) return buttons.reply({ content: `${Symbols.Success} This application is already getting deleted...`, ephemeral: true });
+                            
+                            await Postulation.updateOne({ ChannelID: buttons.channel.id }, { Closed: true });
+                            const DeletedTicket = new EmbedBuilder()
+                            .setDescription(`The application will be deleted in 5s...`)
+                            .setColor(Color.Danger)
+
+                            buttons.channel.send({ embeds: [DeletedTicket] });
+                            setTimeout(async function () {
+                                if(buttons.channel && buttons.guild) {
+                                    buttons.channel.delete();
+                                    await Postulation.deleteOne({ GuildID: buttons.guild.id }, { ChannelID: buttons.channel.id });
+                                }
+                            }, 5000)
+                            
+                            buttons.reply({ content: `${Symbols.Success} The application is being prepared for closing.`, ephemeral: true });
+
+                            break;
+                        
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
