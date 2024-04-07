68e8e7fa579f21b412db527d7608170ddfbe3806
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-07 17:56:13 -0600

    Add options feature and other things

--- apps/vroom/src/events/interactionCreate/ApplicationActions.ts
+++ apps/vroom/src/events/interactionCreate/ApplicationActions.ts
@@ -1,4 +1,4 @@
-import { ButtonInteraction, ChatInputCommandInteraction, EmbedBuilder, Events, PermissionFlagsBits } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ChatInputCommandInteraction, EmbedBuilder, Events, PermissionFlagsBits } from "discord.js";
 import VroomClient from "../../classes/Client";
 import Event from "../../classes/Event";
 import Application from "../../database/models/Application";
@@ -6,11 +6,11 @@ import Postulation from "../../database/models/Postulation";
 import Color from "../../enums/Color";
 import Symbols from "../../enums/Symbols";
 
-export default class ApplicationResponse extends Event {
+export default class ApplicationActions extends Event {
     constructor(client: VroomClient) {
         super(client, {
             name: Events.InteractionCreate,
-            description: "ApplicationResponse Event",
+            description: "ApplicationActions Event",
             once: false
         });
     }
@@ -20,7 +20,7 @@ export default class ApplicationResponse extends Event {
             const buttons = interaction as ButtonInteraction;
 
             if(buttons.guild && buttons.member && buttons.channel && buttons.memberPermissions ) {
-                if(!["close"].includes(buttons.customId)) return;
+                if(!["close", "options"].includes(buttons.customId)) return;
 
                 const data = await Postulation.findOne({ GuildID: buttons.guild.id });
                 if(!buttons.guild.members.me?.permissions.has(PermissionFlagsBits.ManageChannels)) return buttons.reply({ content: `${Symbols.Error} The bot needs permission from \`ManageChannels\` to do this action.` });
@@ -28,7 +28,7 @@ export default class ApplicationResponse extends Event {
                     switch (buttons.customId) {
                         case "close":
                             if (!buttons.memberPermissions.has(PermissionFlagsBits.ManageChannels)) return buttons.reply({ content: `${Symbols.Error} You do not have permission to close this application.`, ephemeral: true });
-                            if (data.Closed === true) return buttons.reply({ content: `${Symbols.Success} This application is already getting deleted...`, ephemeral: true });
+                            if (data.Closed === true) return buttons.reply({ content: `${Symbols.Error} This application is already getting deleted...`, ephemeral: true });
                             
                             await Postulation.updateOne({ ChannelID: buttons.channel.id }, { Closed: true });
                             const DeletedTicket = new EmbedBuilder()
@@ -47,6 +47,30 @@ export default class ApplicationResponse extends Event {
 
                             break;
                         
+                        case "options":
+                            if (!buttons.memberPermissions.has(PermissionFlagsBits.ManageChannels)) return buttons.reply({ content: `${Symbols.Error} You do not have permission to close this application.`, ephemeral: true });
+
+                            const OptionsEmbed = new EmbedBuilder()
+                            .setAuthor({ iconURL: buttons.user.displayAvatarURL(), name: buttons.user.username })
+                            .setTitle("Application Options")
+                            .setColor(Color.Embed)
+                            .setDescription(`Menu of options for applications. To approve or disapprove applications you have to click on the respective buttons.`)
+
+                            const Buttons = new ActionRowBuilder().addComponents(
+                                new ButtonBuilder()
+                                .setCustomId('approve')
+                                .setLabel('Approve')
+                                .setEmoji('<:checkmark:1219293026489339914>')
+                                .setStyle(ButtonStyle.Success),
+
+                                new ButtonBuilder()
+                                .setCustomId('disapprove')
+                                .setLabel('Disapprove')
+                                .setEmoji('<:crossmark:1219293029681201242>')
+                                .setStyle(ButtonStyle.Danger),
+                            )
+
+                            buttons.reply({ embeds: [OptionsEmbed], components: [new ActionRowBuilder<ButtonBuilder>(Buttons)] });
                         default:
                             break;
                     }
