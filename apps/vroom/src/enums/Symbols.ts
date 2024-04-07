d9d5cdc34f02cc3f7d7be8a83f3dd0508db8fe4d
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-07 15:21:57 -0600

    Create Symols.ts

---
+++ apps/vroom/src/enums/Symbols.ts
@@ -0,0 +1,7 @@
+enum Symbols {
+    Success = "<:checkmark:1219293026489339914> \`|\`",
+    Error = "<:crossmark:1219293029681201242> \`|\`",
+    Warning = "⚠ \`|\`",
+}
+
+export default Symbols;
