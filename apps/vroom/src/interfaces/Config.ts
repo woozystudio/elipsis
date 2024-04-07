abf53f9eb82038117262c2e82f53504191d8031f
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-07 15:22:51 -0600

    Create Config.ts

---
+++ apps/vroom/src/interfaces/Config.ts
@@ -0,0 +1,10 @@
+export default interface IConfig {
+    token: string;
+    botId: string;
+    developmentGuildId: string;
+    developmentToken: string;
+    developmentBotId: string;
+    developers: string[];
+    developmentMongooseURL: string;
+    mongooseURL: string;
+}
