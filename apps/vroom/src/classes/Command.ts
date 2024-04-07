27904d51d466f59fd00deee6334826a85bf5971f
Woozy's Studio <woozysstudio23@gmail.com>
2024-04-07 15:20:41 -0600

    Create Command.ts

---
+++ apps/vroom/src/classes/Command.ts
@@ -0,0 +1,35 @@
+import { ChatInputCommandInteraction, CacheType, AutocompleteInteraction } from "discord.js";
+import Category from "../enums/Category";
+import ICommand from "../interfaces/Command";
+import VroomClient from "./Client";
+import ICommandOptions from "../interfaces/CommandOptions";
+
+export default class Command implements ICommand {
+    client: VroomClient;
+    name: string;
+    description: string;
+    category: Category;
+    options: object;
+    default_member_permissions: bigint;
+    dm_permissions: boolean;
+    cooldown: number;
+    development: boolean;
+
+    constructor(client: VroomClient, options: ICommandOptions) {
+        this.client = client;
+        this.name = options.name;
+        this.description = options.description;
+        this.category = options.category;
+        this.options = options.options;
+        this.default_member_permissions = options.default_member_permissions;
+        this.dm_permissions = options.dm_permissions;
+        this.cooldown = options.cooldown;
+        this.development = options.development;
+    }
+
+    Execute(interaction: ChatInputCommandInteraction<CacheType>): void {
+    }
+    AutoComplete(interaction: AutocompleteInteraction<CacheType>): void {
+    }
+    
+}
